bf01cecfbf63b77c85be725fcfdfe3edbb8e7e5e
advx9600 <88348307@163.com>
2015-04-17 16:20:24 +0800

    only when right click Project show the menu

--- src/righclk_opendir/popup/actions/NewAction.java
+++ src/righclk_opendir/popup/actions/NewAction.java
@@ -1,18 +1,20 @@
 package righclk_opendir.popup.actions;
 
+import java.io.IOException;
+
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
-
-import org.eclipse.ui.IFolderLayout;
+import org.eclipse.jdt.internal.core.JavaProject;
 
 public class NewAction implements IObjectActionDelegate {
 
 	private Shell shell;
-	
+	private IStructuredSelection select;
+
 	/**
 	 * Constructor for Action1.
 	 */
@@ -30,17 +32,33 @@ public class NewAction implements IObjectActionDelegate {
 	/**
 	 * @see IActionDelegate#run(IAction)
 	 */
+	/*
+	 * 如果查找不到JavaProject这个类，增加org.eclipse.jdt.internal.core.JavaProject目录
+	 * 这可能是一个Bug
+	 */
 	public void run(IAction action) {
-		MessageDialog.openInformation(
-			shell,
-			"RighClk_OpenDir",
-			"ActionOne was executed.");
+		Object obj = select.getFirstElement();
+		JavaProject pro = (JavaProject) obj;
+		String path = pro.getProject().getParent().getLocation().toOSString()
+				+ pro.getPath().toOSString();
+		// MessageDialog.openInformation(shell, "RighClk_OpenDir",""+path);
+
+		if (path != null && path.length() > 0) {
+			try {
+				Runtime.getRuntime().exec("explorer " + path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			select = (IStructuredSelection) selection;
+		}
 	}
 
 }
